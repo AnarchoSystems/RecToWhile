9571a561d6c6d07e1dd54dd6859a49bf9ee453d0
AnarchoSystems <markus-pfeifer@outlook.com>
2023-06-11 23:45:30 +0200

    api changes

--- Sources/RecToWhile/RecToWhile.swift
+++ Sources/RecToWhile/RecToWhile.swift
@@ -5,46 +5,108 @@
 //  Created by Markus Kasperczyk on 24.01.23.
 //
 
-public enum Continuation<Routine, Return> {
-    case expand(Routine)
-    case call(Routine)
-    case result(Return)
+public enum Continuation<Routine : Algorithm> {
+    
+    case `continue`
+    case call(Routine, (inout Routine, inout Routine.SharedData, Routine.Output) -> Void)
+    case result(Routine.Output)
+    
+    static func call(_ routine: Routine, _ then: @escaping (inout Routine, Routine.Output) -> Void) -> Self {
+        .call(routine) {this, _, result in then(&this, result)}
+    }
 }
 
-public protocol Algorithm {
+public protocol Algorithm<Input, SharedData, Output> {
     
+    associatedtype Input
     associatedtype Output
-    var continuation : Continuation<Self, Output> {get}
-    mutating func onReturn(_ subResult: Output)
+    associatedtype SharedData
+    static func initialize(_ input: Input) -> (Self, SharedData)
+    mutating func step(_ shared: inout SharedData) -> Continuation<Self>
     
 }
 
 public extension Algorithm {
     
-    func run() -> Output {
+    static func run(_ input: Input, stackSize: Int = 1024) -> Output {
         
-        var stack = [Self]()
+        var (this, sharedData) = initialize(input)
+        
+        var stack = [(Self, ((inout Self, inout SharedData, Output) -> Void)?)]()
         
         // just in case that there are a bunch of subroutine calls...
-        stack.reserveCapacity(1024)
+        stack.reserveCapacity(stackSize)
         
-        stack.append(self)
+        stack.append((this, nil))
         
         while true {
-            switch stack[stack.count - 1].continuation {
-            case .expand(let expansion):
-                stack[stack.count - 1] = expansion
-            case .call(let subRoutine):
-                stack.append(subRoutine)
+            switch stack[stack.count - 1].0.step(&sharedData) {
+            case .continue:
+                continue
+            case .call(let subRoutine, let then):
+                stack[stack.count - 1].1 = then
+                stack.append((subRoutine, nil))
             case .result(let subResult):
                 stack.removeLast()
-                if stack.isEmpty {
+                guard let onReturn = stack.last?.1 else {
                     return subResult
                 }
-                stack[stack.count - 1].onReturn(subResult)
+                onReturn(&stack[stack.count - 1].0, &sharedData, subResult)
             }
         }
         
+    }
+    
+}
+
+
+public enum SteppingContinuation<Algo, Data> {
+    case call(Algo)
+    case manip((inout Data) -> Void)
+}
+
+public protocol SteppingAlgo {
+    associatedtype Input
+    associatedtype Data
+    static func initialize(_ input: Input) -> (Self, Data)
+    var steps : [SteppingContinuation<Self, Data>] {get}
+}
+
+public extension SteppingAlgo {
+    typealias Algorithm = Stepper<Self>
+}
+
+public struct Stepper<Algo : SteppingAlgo> : Algorithm {
+    
+    public typealias Output = Algo.Data
+    public typealias SharedData = Algo.Data
+    
+    let algo : Algo
+    var stepIdx = 0
+    
+    public static func initialize(_ input: Algo.Input) -> (Stepper<Algo>, Algo.Data) {
+        let (algo, data) = Algo.initialize(input)
+        return (.init(algo: algo), data)
+    }
+    
+    var currentStep : SteppingContinuation<Algo, Algo.Data>? {
+        algo.steps.indices.contains(stepIdx) ? algo.steps[stepIdx] : nil
+    }
+    
+    public mutating func step(_ shared: inout Algo.Data) -> Continuation<Stepper<Algo>> {
+        guard let currentStep else {
+            return .result(shared)
+        }
+        switch currentStep {
+        case .call(let push):
+            return .call(.init(algo: push)) {this, _ in
+                this.stepIdx += 1
+            }
+        case .manip(let manip):
+            stepIdx += 1
+            manip(&shared)
+            return .continue
+        }
     }
     
 }
--- Tests/RecToWhileTests/RecToWhileTests.swift
+++ Tests/RecToWhileTests/RecToWhileTests.swift
@@ -6,9 +6,9 @@ final class RecToWhileTests: XCTestCase {
     func testEvenOdd() {
         
         for idx in 0..<1000 {
-            XCTAssertEqual(EvenOdd.checkEven(numericCast(idx)).run(),
+            XCTAssertEqual(EvenOdd.run((check: .checkEven, value: numericCast(idx))),
                            idx % 2 == 0)
-            XCTAssertEqual(EvenOdd.checkOdd(numericCast(idx)).run(),
+            XCTAssertEqual(EvenOdd.run((check: .checkOdd, value: numericCast(idx))),
                            idx % 2 == 1)
         }
         
@@ -18,15 +18,16 @@ final class RecToWhileTests: XCTestCase {
         
         for size in 3...10 {
             do {
-                let hanoi = Hanoi(size)
-                solveHanoi(hanoi)
+                var hanoi = Hanoi(size)
+                solveHanoi(&hanoi)
                 XCTAssert(hanoi.isSolved, "pile 1: \(hanoi.pile1), pile 2: \(hanoi.pile2), pile3: \(hanoi.pile3)")
                 
             }
             
             do {
-                let hanoi = HanoiSolver(game: Hanoi(size)).run()
-                XCTAssert(hanoi.isSolved, "pile 1: \(hanoi.pile1), pile 2: \(hanoi.pile2), pile3: \(hanoi.pile3)")
+                let hanoi = SimpleHanoiSolver.Algorithm.run(Hanoi(size))
+                XCTAssert(hanoi.isSolved,
+                          "pile 1: \(hanoi.pile1), pile 2: \(hanoi.pile2), pile3: \(hanoi.pile3)")
             }
         }
     }
@@ -34,10 +35,26 @@ final class RecToWhileTests: XCTestCase {
     func testAckermann() {
         
         for i in 0...3 {
-            for j in 0...9 {
+            for j in 0...13 {
                 XCTAssertEqual(ackermannRec(numericCast(i), numericCast(j)),
-                               Ackermann.requested(n: numericCast(i), m: .computed(numericCast(j))).run())
+                               Ackermann.run(.init(n: numericCast(i), m: numericCast(j))))
             }
+        }
+        
+    }
+    
+    func fib(req: Int) -> (Int, Int) {
+        if req <= 1 {return (1, 1)}
+        let (nMin1, nMin2) = fib(req: req - 1)
+        return (nMin1 + nMin2, nMin1)
+    }
+    
+    func testFib() {
+        
+        for idx in 0..<50 {
+            let res1 = fib(req: idx).0
+            let res2 = Fibonacci.run(idx).last!
+            XCTAssertEqual(res1, res2)
         }
         
     }
@@ -46,31 +63,36 @@ final class RecToWhileTests: XCTestCase {
 
 enum EvenOdd : Algorithm {
     
-    case checkEven(UInt)
-    case checkOdd(UInt)
+    typealias Output = Bool
     
-    var continuation: Continuation<EvenOdd, Bool> {
+    case checkEven
+    case checkOdd
+    
+    mutating func toggle() {
         switch self {
-        case .checkEven(let int):
-            if int == 0 {
-                return .result(true)
-            }
-            return .expand(.checkOdd(int - 1))
-        case .checkOdd(let int):
-            if int == 0 {
-                return .result(false)
-            }
-            return .expand(.checkEven(int - 1))
+        case .checkEven:
+            self = .checkOdd
+        case .checkOdd:
+            self = .checkEven
         }
     }
     
-    mutating func onReturn(_ subResult: Bool) {
-        fatalError()
+    static func initialize(_ input: (EvenOdd, UInt)) -> (EvenOdd, UInt) {
+        input
+    }
+    
+    mutating func step(_ int: inout UInt) -> Continuation<EvenOdd> {
+        if int == 0 {
+            return .result(self == .checkEven)
+        }
+        toggle()
+        int -= 1
+        return .continue
     }
     
 }
 
-final class Hanoi {
+struct Hanoi {
     
     private(set) var pile1 : [Int]
     private(set) var pile2 = [Int]()
@@ -109,13 +131,11 @@ final class Hanoi {
         }
     }
     
-    @discardableResult
-    func move(from: PileIdx, to: PileIdx) -> Hanoi {
-        guard !self[from].isEmpty else {return self}
-        guard self[to].isEmpty || self[from].last! > self[to].last! else {return self}
+    mutating func move(from: PileIdx, to: PileIdx) {
+        guard !self[from].isEmpty else {return}
+        guard self[to].isEmpty || self[from].last! > self[to].last! else {return}
         let elm = self[from].removeLast()
         self[to].append(elm)
-        return self
     }
     
     var isSolved : Bool {
@@ -124,11 +144,15 @@ final class Hanoi {
     
 }
 
-func solveHanoi(_ hanoi: Hanoi) {
-    moveAll(hanoi: hanoi, amount: hanoi.pile1.count, from: .first, to: .third, via: .second)
+func solveHanoi(_ hanoi: inout Hanoi) {
+    moveAll(hanoi: &hanoi,
+            amount: hanoi.pile1.count,
+            from: .first,
+            to: .third,
+            via: .second)
 }
 
-func moveAll(hanoi: Hanoi,
+func moveAll(hanoi: inout Hanoi,
              amount: Int,
              from: Hanoi.PileIdx,
              to: Hanoi.PileIdx,
@@ -136,126 +160,138 @@ func moveAll(hanoi: Hanoi,
     if amount <= 0 {
         return
     }
-    moveAll(hanoi: hanoi, amount: amount - 1, from: from, to: via, via: to)
+    moveAll(hanoi: &hanoi, amount: amount - 1, from: from, to: via, via: to)
     hanoi.move(from: from, to: to)
-    moveAll(hanoi: hanoi, amount: amount - 1, from: via, to: to, via: from)
+    moveAll(hanoi: &hanoi, amount: amount - 1, from: via, to: to, via: from)
 }
 
-struct HanoiSolver : Algorithm {
+struct SimpleHanoiSolver : SteppingAlgo {
     
-    let game : Hanoi
-    var state : State = .newGame
+    let from : Hanoi.PileIdx
+    let to : Hanoi.PileIdx
+    let via : Hanoi.PileIdx
+    let amount : Int
     
-    enum State {
-        case newGame
-        case moving(Move)
-        case done
+    static func initialize(_ input: Hanoi) -> (SimpleHanoiSolver, Hanoi) {
+        (.init(from: .first,
+               to: .third,
+               via: .second,
+               amount: input.pile1.count),
+         input)
     }
     
-    enum MoveStage {
-        case first, second, third
-    }
-    
-    struct Move {
-        let amount : Int
-        let from : Hanoi.PileIdx
-        let to : Hanoi.PileIdx
-        let via : Hanoi.PileIdx
-        var stage : MoveStage = .first
-    }
-    
-    var continuation: Continuation<HanoiSolver, Hanoi> {
-        switch state {
-        case .newGame:
-            return .expand(HanoiSolver(game: game,
-                                       state: .moving(Move(amount: game.pile1.count,
-                                                           from: .first,
-                                                           to: .third,
-                                                           via: .second))))
-        case .moving(var move):
-            if move.amount <= 0 {
-                return .result(game)
-            }
-            switch move.stage {
-            case .first:
-                return .call(HanoiSolver(game: game,
-                                         state: .moving(Move(amount: move.amount - 1,
-                                                             from: move.from,
-                                                             to: move.via,
-                                                             via: move.to))))
-            case .second:
-                move.stage = .third
-                return .expand(HanoiSolver(game: game.move(from: move.from, to: move.to),
-                                           state: .moving(move)))
-            case .third:
-                return .call(HanoiSolver(game: game,
-                                         state: .moving(Move(amount: move.amount - 1,
-                                                             from: move.via,
-                                                             to: move.to,
-                                                             via: move.from))))
-            }
-        case .done:
-            return .result(game)
+    var steps : [SteppingContinuation<Self, Hanoi>] {
+        if amount <= 0 {
+            return []
         }
-    }
-    
-    mutating func onReturn(_ subResult: Hanoi) {
-        guard case .moving(var move) = state else {
-            fatalError()
-        }
-        switch move.stage {
-        case .first:
-            move.stage = .second
-            state = .moving(move)
-        case .second:
-            fatalError()
-        case .third:
-            state = .done
-        }
+        return [
+            .call(.init(from: from,
+                        to: via,
+                        via: to,
+                        amount: amount - 1)),
+            
+                .manip{hanoi in hanoi.move(from: from, to: to)},
+            
+                .call(.init(from: via,
+                            to: to,
+                            via: from,
+                            amount: amount - 1))
+        ]
     }
     
 }
 
 func ackermannRec(_ n: UInt, _ m: UInt) -> UInt {
+    var lookUp = [IntPair:UInt]()
+    return ackermannRec(n, m, lookup: &lookUp)
+}
+
+func ackermannRec(_ n: UInt, _ m: UInt, lookup: inout [IntPair : UInt]) -> UInt {
+    
+    if let memo = lookup[IntPair(n: n, m: m)] {
+        return memo
+    }
     
     if n == 0 {
         return m+1
     }
     if m == 0 {
-        return ackermannRec(n-1, 1)
+        return ackermannRec(n-1, 1, lookup: &lookup)
     }
-    return ackermannRec(n-1, ackermannRec(n, m-1))
+    let res1 = ackermannRec(n, m-1, lookup: &lookup)
+    lookup[IntPair(n: n, m: m-1)] = res1
+    let res2 = ackermannRec(n-1, res1, lookup: &lookup)
+    lookup[IntPair(n: n-1, m: res1)] = res2
+    return res2
     
 }
 
-enum Ackermann : Algorithm {
+struct IntPair : Hashable {
+    let n : UInt
+    let m : UInt
+}
+
+enum Ackermann : Algorithm, Hashable {
+    
+    typealias Output = UInt
+    
+    static func initialize(_ input: IntPair) -> (Ackermann, [Ackermann : UInt]) {
+        (.requested(n: input.n, m: .computed(input.m)), [:])
+    }
     
     indirect case requested(n: UInt, m: Ackermann)
     case computed(UInt)
     
-    var continuation: Continuation<Ackermann, UInt> {
+    mutating func step(_ lookup: inout [Ackermann : UInt]) -> Continuation<Ackermann> {
+        if let memo = lookup[self] {
+            return .result(memo)
+        }
         switch self {
+        case .computed(let result):
+            return .result(result)
         case .requested(let n, let m):
             guard case .computed(let m) = m else {
-                return .call(m)
+                return .call(m) {this, lookup, result in
+                    this = .requested(n: n, m: .computed(result))
+                    lookup[m] = result
+                }
             }
             if n == 0 {
+                lookup[self] = m+1
                 return .result(m+1)
             }
             if m == 0 {
-                return .expand(.requested(n: n-1, m: .computed(1)))
+                self = .requested(n: n-1, m: .computed(1))
+                return .continue
             }
-            return .expand(.requested(n: n-1, m: .requested(n: n, m: .computed(m-1))))
-        case .computed(let result):
-            return .result(result)
+            self = .requested(n: n-1, m: .requested(n: n, m: .computed(m-1)))
+            return .continue
         }
     }
     
-    mutating func onReturn(_ subResult: UInt) {
-        guard case .requested(let n, _) = self else {
-            fatalError()
+}
+
+struct Fibonacci : Algorithm {
+    
+    typealias Output = [Int]
+    
+    init(_ val: Int) {
+        remainingIterations = val - 1
+    }
+    
+    var remainingIterations : Int
+    
+    static func initialize(_ input: Int) -> (Fibonacci, [Int]) {
+        (.init(input), [1, 1])
+    }
+    
+    mutating func step(_ numbers: inout [Int]) -> Continuation<Fibonacci> {
+        if remainingIterations <= 0 {
+            return .result(numbers)
         }
-        self = .requested(n: n, m: .computed(subResult))
+        remainingIterations -= 1
+        numbers.append(numbers.last! + numbers.dropLast().last!)
+        return .continue
     }
     
 }
